4e30aaf01f91cf7f6796bcbcd8955085f8fe8e75
dillujr <125227926+dillujr@users.noreply.github.com>
2023-10-11 17:13:59 +0000

    new update

--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 
-    # path('accounts/',include('django.contrib.auth.urls'))
+ 
 ]
